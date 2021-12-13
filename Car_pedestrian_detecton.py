34e891be8daf27c740b2562234a4baf5baf620ad
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-12-13 14:40:47 +0545

    Car Image Identification

--- Car_pedestrian_detecton.py
+++ Car_pedestrian_detecton.py
@@ -1,7 +1,7 @@
 import cv2
 
 #Our Image
-img_file = 'tesla.jpg'
+img_file = 'car.jpg'
 
 #Pre-trained car classifier
 classifier_file = 'car_detector.xml'
@@ -15,8 +15,16 @@ black_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #create car classifier
 car_tracker = cv2.CascadeClassifier(classifier_file)
 
+#detect cars
+cars = car_tracker.detectMultiScale(black_white)
+print(cars)
+
+#draw rectangles around the cars
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x,y),(x+w, y+h),(0, 0, 255),2)
+
 #Display the imgae 
-cv2.imshow('Car detector',black_white)
+cv2.imshow('Car detector',img)
 
 
 cv2.waitKey()
